2fc2ae76e7f080565bc3b3564bf7ddbf141bbd7f
shanthanb <shanthanboppudi@gmail.com>
2020-09-24 17:13:54 +0530

    Updates

---
+++ precache-manifest.44d5004e5aac425f46356500c1bfbd9c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdd2d42933c1564b6436cfd01c72b35f",
+    "url": "/binary-task/index.html"
+  },
+  {
+    "revision": "8be5d12e674187268cd4",
+    "url": "/binary-task/static/css/2.c11e63b2.chunk.css"
+  },
+  {
+    "revision": "aa24a642a2045c81218e",
+    "url": "/binary-task/static/css/main.b9e1b59f.chunk.css"
+  },
+  {
+    "revision": "8be5d12e674187268cd4",
+    "url": "/binary-task/static/js/2.185006c4.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/binary-task/static/js/2.185006c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa24a642a2045c81218e",
+    "url": "/binary-task/static/js/main.c2e044de.chunk.js"
+  },
+  {
+    "revision": "0e1cfbe0d2e806cd5438",
+    "url": "/binary-task/static/js/runtime-main.88214496.js"
+  },
+  {
+    "revision": "189e324b1b4deb792b85b6df6ee6c8a4",
+    "url": "/binary-task/static/media/app-store-badge.189e324b.svg"
+  },
+  {
+    "revision": "b775eda8f6031baaad8721debd4a00aa",
+    "url": "/binary-task/static/media/google-play-badge.b775eda8.svg"
+  }
+]);
